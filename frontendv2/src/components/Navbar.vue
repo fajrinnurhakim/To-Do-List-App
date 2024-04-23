0bb40677007ebacba7005d89522f5d9404cbd72f
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-04-24 00:17:28 +0700

    add: vue setup

---
+++ frontendv2/src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">home</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+    </nav>
+  </div>
+</template>
